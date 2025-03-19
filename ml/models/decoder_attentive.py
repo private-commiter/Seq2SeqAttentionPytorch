eb9cdd755032c51ae68f628c635362042ecee72d
private.commiter <private.commiter@nomail.com>
2025-03-20 02:03:59 +0400

    refactor ml folder structure

--- ml/models/decoder_attentive.py
+++ ml/models/decoder_attentive.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as nn_func
+import torch.nn.functional as nn_functional
 
 
 class BahdanauAttention(nn.Module):
@@ -14,7 +14,7 @@ class BahdanauAttention(nn.Module):
         scores = self.Va(torch.tanh(self.Wa(query) + self.Ua(keys)))
         scores = scores.squeeze(2).unsqueeze(1)
 
-        weights = nn_func.softmax(scores, dim=-1)
+        weights = nn_functional.softmax(scores, dim=-1)
         context = torch.bmm(weights, keys)
 
         return context, weights
@@ -24,10 +24,10 @@ class AttentiveDecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size, dropout_p=0.1):
         super(AttentiveDecoderRNN, self).__init__()
         self.embedding = nn.Embedding(output_size, hidden_size)
+        self.dropout = nn.Dropout(dropout_p)
         self.attention = BahdanauAttention(hidden_size)
         self.gru = nn.GRU(2 * hidden_size, hidden_size, batch_first=True)
         self.out = nn.Linear(hidden_size, output_size)
-        self.dropout = nn.Dropout(dropout_p)
 
     def forward(self, encoder_outputs, encoder_hidden, sos_token, device, target_tensor=None, max_length=10):
         batch_size = encoder_outputs.size(0)
@@ -64,7 +64,7 @@ class AttentiveDecoderRNN(nn.Module):
                 decoder_input = topi.squeeze(-1).detach()  # detach from history as input
 
         decoder_outputs = torch.cat(decoder_outputs, dim=1)
-        decoder_outputs = nn_func.log_softmax(decoder_outputs, dim=-1)
+        decoder_outputs = nn_functional.log_softmax(decoder_outputs, dim=-1)
         attentions = torch.cat(attentions, dim=1)
 
         return decoder_outputs, decoder_hidden, attentions
